ab55ca0ef0120f14c9a93bc5d57f27efa31a4a56
ckolbegger <castaway@kolbegger.com>
2022-07-06 18:07:19 -0400

    Using Spring DI with Qualifiers

---
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -0,0 +1,26 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    public GreetingService getGreetingService() {
+        return greetingService;
+    }
+
+    @Qualifier("setterInjectedGreetingService")
+    @Autowired
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
