82229b1b33e8ac36b8dca15662fe514e911d65c3
ckolbegger <castaway@kolbegger.com>
2022-07-07 12:32:03 -0400

    Example of how to specify something is part of the default profile

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("I18nService")
 public class I18nSpanishGreetingService implements GreetingService {
     @Override
